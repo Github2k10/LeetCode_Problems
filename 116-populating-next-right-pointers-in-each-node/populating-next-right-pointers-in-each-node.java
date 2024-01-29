e8c5f59f696ca1f9ab5305334089432234de6f24
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-01-30 00:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (23.78%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -24,30 +24,19 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root == null) return null;
-        List<List<Node>> list = new ArrayList<>();
-        
-        helper(root, 0, list);
+        if(root.left == null && root.right == null) return root;
 
-        for(List<Node> l : list){
-            for(int i = 0; i < l.size(); i++){
-                if(i == l.size() - 1){
-                    l.get(i).next = null;
-                } else {
-                    l.get(i).next = l.get(i + 1);
-                }
-            }
+        root.left.next = root.right;
+
+        if(root.next == null){
+            root.right.next = null;
+        } else {
+            root.right.next = root.next.left;
         }
 
+        connect(root.left);
+        connect(root.right);
+
         return root;
-    }
-
-    private void helper(Node root, int l, List<List<Node>> list){
-        if(root == null) return;
-        if(l == list.size()) list.add(new ArrayList<>());
-
-        list.get(l).add(root);
-
-        helper(root.left, l + 1, list);
-        helper(root.right, l + 1, list);
     }
 }
