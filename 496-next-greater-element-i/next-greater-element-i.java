be50e0220bc672de9ad581d20c7c72240fd50e20
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-01-31 19:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (8.03%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for(int i = 0; i < nums1.length; i++){
-            int j = 0;
-            
-            while(j < nums2.length){
-                if(nums1[i] == nums2[j]) break;
-                j++;
-            }
-            
-            while(j < nums2.length){
-                if(nums2[j] > nums1[i]){
-                    nums1[i] = nums2[j];
-                    break;
-                }
-                j++;
-            }
-            if(j >= nums2.length){
-                nums1[i] = -1;
-            }
+        int[] arr = new int[10001];
+        for(int i = 0; i < nums2.length; i++){
+            arr[nums2[i]] = i;
         }
 
+        for(int i = 0; i < nums1.length; i++){
+            nums1[i] = helper(arr[nums1[i]], nums2);
+        }
         return nums1;
+    }
+
+    private int helper(int k, int[] arr){
+        int j = k + 1;
+
+        while(j < arr.length){
+            if(arr[j] > arr[k]) return arr[j];
+            j++;
+        }
+        return -1;
     }
 }
