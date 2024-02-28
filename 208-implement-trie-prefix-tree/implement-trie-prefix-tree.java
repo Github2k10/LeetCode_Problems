0e02389b00c21610f7d432eee3bfd55c2a630487
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-02-28 23:23:53 +0530

    Time: 406 ms (5.00%) | Memory: 52.2 MB (99.36%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,30 @@
+class Trie {
+    private List<String> list;
+    public Trie() {
+        list = new ArrayList<>();
+    }
+    
+    public void insert(String word) {
+        list.add(word);
+    }
+    
+    public boolean search(String word) {
+        return list.contains(word);
+    }
+    
+    public boolean startsWith(String prefix) {
+        for(String s : list){
+            if(s.indexOf(prefix) == 0) return true;
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
