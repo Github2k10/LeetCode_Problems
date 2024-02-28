ce3d4fd7f05947584bbacd7d488055a84ecd794a
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-02-28 23:51:10 +0530

    Time: 37 ms (63.31%) | Memory: 55.2 MB (58.75%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,23 +1,72 @@
 class Trie {
-    private List<String> list;
+    private TrieNode root;
+
     public Trie() {
-        list = new ArrayList<>();
+        root = new TrieNode();
     }
     
     public void insert(String word) {
-        list.add(word);
+        int level;
+        int index;
+        int len = word.length();
+
+        TrieNode node = root;
+
+        for(level = 0; level < len; level++){
+            index = word.charAt(level) - 'a';
+            if(node.child[index] == null){
+                node.child[index] = new TrieNode();
+            }
+            node = node.child[index];
+        }
+
+        node.isEnd = true;
     }
     
     public boolean search(String word) {
-        return list.contains(word);
+        int level;
+        int index;
+        int len = word.length();
+
+        TrieNode node = root;
+
+        for(level = 0; level < len; level++){
+            index = word.charAt(level) - 'a';
+            if(node.child[index] == null){
+                return false;
+            }
+            node = node.child[index];
+        }
+
+        return node.isEnd;
     }
     
     public boolean startsWith(String prefix) {
-        for(String s : list){
-            if(s.indexOf(prefix) == 0) return true;
+        int index;
+        int level;
+        int len = prefix.length();
+
+        TrieNode node = root;
+
+        for(level = 0; level < len; level++){
+            index = prefix.charAt(level) - 'a';
+            if(node.child[index] == null) return false;
+            node = node.child[index];
         }
 
-        return false;
+        return true;
+    }
+}
+
+class TrieNode{
+    boolean isEnd;
+    TrieNode[] child = new TrieNode[26];
+
+    TrieNode(){
+        isEnd = false;
+        for(int i = 0; i < 26; i++){
+            child[i] = null;
+        }
     }
 }
 
