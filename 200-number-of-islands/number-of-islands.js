af4be313ab7ba6b5c097715ec843a76d717c3073
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-04-19 21:12:49 +0530

    Time: 75 ms (53.07%) | Memory: 52.1 MB (78.83%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -5,6 +5,18 @@
 var numIslands = function(grid) {
     let c = 0;
 
+    const helper = (i, j) => {
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return;
+        if(grid[i][j] == 0) return;
+
+        grid[i][j] = 0;
+
+        helper(i - 1, j, grid);
+        helper(i + 1, j, grid);
+        helper(i, j - 1, grid);
+        helper(i, j + 1, grid);
+    }
+
     for(let i = 0; i < grid.length; i++){
         for(let j = 0; j < grid[0].length; j++){
             if(grid[i][j] == 1){
@@ -16,15 +28,3 @@ var numIslands = function(grid) {
 
     return c;
 };
-
-const helper = (i, j, grid) => {
-    if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return;
-    if(grid[i][j] == 0) return;
-
-    grid[i][j] = 0;
-
-    helper(i - 1, j, grid);
-    helper(i + 1, j, grid);
-    helper(i, j - 1, grid);
-    helper(i, j + 1, grid);
-}
