8ec6b6241470c41d6c5f7ddc472f921ac8f371bf
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-04-20 11:03:59 +0530

    Time: 247 ms (35.56%) | Memory: 93.8 MB (15.56%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[][]} land
+ * @return {number[][]}
+ */
+var findFarmland = function(land) {
+    const farms = [];
+    let n = land.length, m = land[0].length;
+
+    const helper = (i, j, arr) => {
+        if(i < 0 || j < 0 || i >= n || j >= m) return;
+        if(land[i][j] == 0) return;
+
+        if(land[i][j] == 1){
+            if(i > arr[2]) arr[2] = i;
+            if(j > arr[3]) arr[3] = j;
+        }
+
+        land[i][j] = 0;
+
+        helper(i + 1, j, arr);
+        helper(i - 1, j, arr);
+        helper(i, j + 1, arr);
+        helper(i, j - 1, arr);
+    }
+
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            if(land[i][j] == 1){
+                const arr = [];
+                arr.push(i);
+                arr.push(j);
+                arr.push(i);
+                arr.push(j);
+
+                helper(i, j, arr);
+                farms.push(arr);
+            }
+        }
+    }
+
+    return farms;
+};
