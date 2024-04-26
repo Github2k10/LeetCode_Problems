ef7cfbf5fb10bcd5443ac8dd6342f2f72a9a60ce
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2024-04-26 14:54:04 +0530

    Time: 31 ms (62.59%) | Memory: 51.1 MB (53.06%) - LeetSync

---
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    private int[][] dp;
+
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        dp = new int[n][n];
+
+        for(int i = 0; i < n; i++) dp[0][i] = grid[0][i];
+        for(int i = 1; i < n; i++) Arrays.fill(dp[i], -1);
+
+        helper(1, n, grid);
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            ans = Math.min(dp[n - 1][i], ans);
+        }
+
+
+        return ans;
+    }
+
+    private void helper(int row, int n, int[][] grid){
+        if(row == n) return;
+
+        for(int i = 0; i < n; i++){
+            int min = Integer.MAX_VALUE;
+
+            for(int j = 0; j < n; j++){
+                if(i != j)
+                    min = Math.min(dp[row - 1][j], min);
+            }
+
+            dp[row][i] = grid[row][i] + min;
+        }
+
+        helper(row + 1, n, grid);
+    }
+}
