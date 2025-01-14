4ab2ce2db55590447eee9f1519f424a47ac71d3c
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2025-01-14 09:54:22 +0530

    Time: 2 ms (95.83%) | Memory: 45.9 MB (16.93%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -6,17 +6,8 @@ class Solution {
         int count = 0;
         
         for(int i = 0; i < n; i++){
-            if(arr[A[i] - 1] == 0) arr[A[i] - 1]++;
-            else if(arr[A[i] - 1] <= 2) {
-                arr[A[i] - 1]++;
-                if(arr[A[i] - 1] % 2 == 0) count++;
-            }
-            
-            if(arr[B[i] - 1] == 0) arr[B[i] - 1]++;
-            else if(arr[B[i] - 1] <= 2) {
-                arr[B[i] - 1]++;
-                if(arr[B[i] - 1] % 2 == 0) count++;
-            }
+            if(++arr[A[i] - 1] == 2) count++;
+            if(++arr[B[i] - 1] == 2) count++;
 
             ans[i] = count;
         }
